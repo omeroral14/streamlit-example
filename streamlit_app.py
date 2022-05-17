5e7a588e652a6cbf1554221b055c03d92d6f7d28
omeroral14 <105738586+omeroral14@users.noreply.github.com>
2022-05-17 19:52:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,53 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+import numpy as np
+from sklearn.tree import export_graphviz
 
-"""
-# Welcome to Streamlit!
+pokemon=pd.read_csv("pokemon.csv")
+combats=pd.read_csv("combats.csv")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+col1,col2,col3=st.columns(3)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+with col1:
+    poke1=st.selectbox("İlk Pokemon", pokemon['Name'])
+    isim=poke1.lower()
+    isim="images/images/"+isim+".png"
+    st.image(isim)
+    poke1 = int(pokemon[pokemon["Name"] == poke1]["#"])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
+with col2:
+    st.image("https://www.nicepng.com/png/full/271-2712237_vs-rooster-teeth.png")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+with col3:
+    poke2 = st.selectbox("İkinci Pokemon", pokemon['Name'])
+    isim = poke2.lower()
+    isim = "images/images/" + isim + ".png"
+    st.image(isim)
+    poke2 = int(pokemon[pokemon["Name"] == poke2]["#"])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+combats['Winner']=combats['First_pokemon']==['Winner']
+combats['Winner']=np.where(combats['Winner']==True , 0 , 1)
 
-    points_per_turn = total_points / num_turns
+y=combats[["Winner"]]
+x=combats[["First_pokemon","Second_pokemon"]]
+tree=DecisionTreeClassifier()
+model=tree.fit(x,y)
+skor=model.score(x,y)
+tahmin=model.predict([[poke1,poke2]])
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+kol1,kol2,kol3=st.columns(3)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+with kol1:
+    st.write("")
+with kol2:
+    buton=st.button("Başlat")
+    if buton:
+        if tahmin==0:
+            st.header("Kazanan")
+            st.subheader(isim1)
+        else:
+            st.header("Kazanan")
+            st.subheader(isim2)
+        st.balloons()
