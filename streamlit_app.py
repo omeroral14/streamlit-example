17ed57782c4761196460364c3d3bf34db64998d5
omeroral14 <105738586+omeroral14@users.noreply.github.com>
2022-05-17 19:58:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ from collections import  namedtuple
 import altair as alt
 import math
 import pandas as pd
-from sklearn.tree import export_graphviz
+import streamlit as st
 
 st.title("Kaç yaşındasın")
 dogum=st.number_input("Doğum Tarihiniz")
