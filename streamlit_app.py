df090f1a0c3ffdec77a6d910dd5ec36f563b35bd
omeroral14 <105738586+omeroral14@users.noreply.github.com>
2022-05-17 20:14:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from sklearn.tree import Decisi
 
 st.title("Kaç yaşındasın")
 dogum=st.number_input("Doğum Tarihiniz")
